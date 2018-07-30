9b51a4baebf22124206c5468a6e7fcaf1b7e6156
debbie <deborah7229@gmail.com>
2018-07-30 13:13:00 +0800

    1

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(2222);
+            Console.WriteLine(123456);
+            Console.WriteLine(567);
+            Console.WriteLine(1111111);
+        }
+    }
+}
